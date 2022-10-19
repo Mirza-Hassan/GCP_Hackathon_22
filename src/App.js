6a9ed6add988def286ea3535b399f3a1add43674
Mirza-Hassan <hassanbarlas50@gmail.com>
2022-10-20 02:49:37 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2> React GCP Hackathon</h2>
+    </div>
+  );
+}
+
+export default App;
+
